24c83d6a96799fa787179c171b2c7f63a1d957d0
Prithvi Dev <prithvidevkanojia@gmail.com>
2020-10-05 18:27:14 +0530

    Upload

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -47,5 +48,136 @@ public class MainActivity extends AppCompatActivity {
 
         inputArea=findViewById(R.id.inputArea);
         outputArea=findViewById(R.id.outputArea);
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputArea.setText("");
+                outputArea.setText("");
+            }
+        });
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"0");
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"2");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"3");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"4");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"5");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"6");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"7");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"8");
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"9");
+            }
+        });
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"+");
+            }
+        });
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"-");
+            }
+        });
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"x");
+            }
+        });
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"÷");
+            }
+        });
+        btnDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                process = inputArea.getText().toString();
+                inputArea.setText(process+".");
+            }
+        });
+        btnBrackets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean br = true;
+                if(br){
+                process = inputArea.getText().toString();
+                inputArea.setText(process+"(");
+                br = false;
+                }
+                else{
+                    process = inputArea.getText().toString();
+                    inputArea.setText(process+")");
+                    br = true;
+                }
+            }
+        });
+
     }
 }
