edff4bf91d56a5460a374c94210466dea0ea68de
Prithvi Dev <prithvidevkanojia@gmail.com>
2020-10-06 19:21:34 +0530

    Upload

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import java.util.*;
 import android.os.ConditionVariable;
 import android.view.View;
 import android.view.Window;
@@ -196,13 +197,77 @@ public class MainActivity extends AppCompatActivity {
                 process = process.replaceAll("x","*");
                 process = process.replaceAll("÷","/");
                 process = process.replaceAll("%","/100");
-                char[] s = process.toCharArray();
-                for(int i =0; i<=s.length; i++){
 
+                char[] tokens = inputArea.getText().toString().toCharArray();
+
+                Stack<Integer> values = new Stack<Integer>();
+                Stack<Character> ops = new Stack<Character>();
+
+                for(int i=0; i<tokens.length;i++){
+                    if(tokens[i] ==' '){
+                        continue;
+                    }
+                    if (tokens[i] >= '0' && tokens[i] <= '9')
+                    {
+                        StringBuffer sbuf = new StringBuffer();
+                        // There may be more than one digits in number
+                        while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
+                            sbuf.append(tokens[i++]);
+                        values.push(Integer.parseInt(sbuf.toString()));
+                    }
+                    else if (tokens[i] == '(')
+                        ops.push(tokens[i]);
+                    else if (tokens[i] == ')')
+                    {
+                        while (ops.peek() != '(')
+                            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                        ops.pop();
+                    }
+                    else if (tokens[i] == '+' || tokens[i] == '-' ||
+                            tokens[i] == '*' || tokens[i] == '/')
+                    {
+
+                        while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+                            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+                        ops.push(tokens[i]);
+                    }
                 }
+                while (!ops.empty())
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                inputArea.setText(values.pop());
+        }
 
+            public boolean hasPrecedence(char op1, char op2)
+            {
+                if (op2 == '(' || op2 == ')')
+                    return false;
+                if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+                    return false;
+                else
+                    return true;
+            }
+
+            public int applyOp(char op, int b, int a)
+            {
+                switch (op)
+                {
+                    case '+':
+                        return a + b;
+                    case '-':
+                        return a - b;
+                    case '*':
+                        return a * b;
+                    case '/':
+                        if (b == 0)
+                            throw new
+                                    UnsupportedOperationException("Cannot divide by zero");
+                        return a / b;
+                }
+                return 0;
             }
         });
+
 
     }
 }
